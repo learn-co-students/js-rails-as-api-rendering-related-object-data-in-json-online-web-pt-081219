4ab9a0fb0e843ce00ffea145df2824e87b0661a5
kierra <51520037+kierrakay@users.noreply.github.com>
2020-04-28 18:05:10 -0400

    Done.

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,21 +1,11 @@
 class SightingsController < ApplicationController
 
-    def show
-        sighting = Sighting.find_by(id: params[:id])
-        if sighting 
-        render json: sighting, include: [:bird, :location]
-        # render json: { id: sighting.id, bird: sighting.bird, location: sighting.location }
-#above is nested. starting with sighting aving two keys of bird amd location
-        else 
-            render json: { message: 'No sighting found with that id' }
-        end
-end
-end
 
-# def show
-#     sighting = Sighting.find_by(id: params[:id])
-#     render json: sighting.to_json(:include => {
-#       :bird => {:only => [:name, :species]},
-#       :location => {:only => [:latitude, :longitude]}
-#     }, :except => [:updated_at])
-#   end
+def show
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+  end
+end
