9f37ffb521c976b9ac461caa8aaa7fae60a4483a
github username <github email address>
2020-06-19 10:18:23 -0400

    Done.

---
+++ app/controllers/sightings_controller.rb
@@ -0,0 +1,7 @@
+  
+class SightingsController < ApplicationController
+    def index
+        sightings = Sighting.all
+        render json: sightings.to_json(include: [:bird, :location])
+      end
+end
